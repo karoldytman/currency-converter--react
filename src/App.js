11ec2637f614247687bcea076473cda1817c1792
Karol Dytman <dytmankarol@wp.pl>
2023-04-18 16:58:17 +0200

    interaction app

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@ import Container from "./Container";
 import Form from "./Form";
 import Footer from "./Footer";
 import Header from "./Header";
+import { useState } from "react";
 
 function App() {
+
+  const [result, setResult] = useState(null);
+  const calculateExchange = (currency, amount) => {
+    const rate = currency?.ratio;
+    setResult({
+      sorceAmount: +amount,
+      targetAmount: amount / rate,
+      currency,
+    });
+ 
+  }
   return (
 
     <Container>
