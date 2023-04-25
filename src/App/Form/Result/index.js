18bc4cb45d046ac8a074a2de4e13106618c470b6
Karol Dytman <dytmankarol@wp.pl>
2023-04-25 14:03:59 +0200

    adding result another place

--- src/App/Form/Result/index.js
+++ src/App/Form/Result/index.js
@@ -1,11 +1,14 @@
+import "./style.css";
 export const Result = ({ result }) => (
-   <p> {result !== undefined && (
+   <p className="result"> 
+   Kwota po przeliczeniu:
+    {result !== undefined && (
         <>
-        {result.sourceAmount.toFixed(2)}
+        {result.sourceAmount.toFixed(2)}&nbsc;PLN&nbsc;=&nbsc;
         <strong>
-            {result.targetAmount.toFixed(2)}{result.currency}
+            {result.targetAmount.toFixed(2)}&nbsc;{result.currency}
             </strong> 
             </>
-             )}
+             )} 
 </p>
 );
